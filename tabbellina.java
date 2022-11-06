861298dce38d9fadd7330e333eff4c33e3f44661
nandojv <116838266+nandojv@users.noreply.github.com>
2022-11-06 15:45:36 +0100

    Add files via upload

---
+++ tabbellina.java
@@ -0,0 +1,17 @@
+package corsoiniziodacapo;
+
+public class tabbellina {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("\n Matematica");
+		
+		for(int x = 1; x<=10; x++) {System.out.println("\n  tabellina del: "+x);
+	for(int y=1;y<=10; y++) {
+		System.out.print(x*y );
+	}
+
+}
+
+	}}
